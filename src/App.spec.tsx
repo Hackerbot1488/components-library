5000ea5c6a6d7db2c2c8f607f063cf3da42c4f47
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-16 16:21:26 +0300

    rename app test

---
+++ src/App.spec.tsx
@@ -0,0 +1,9 @@
+import { shallow } from "enzyme";
+import React from "react";
+import App from "./App";
+describe("App component", () => {
+	it("should render App component", () => {
+		const component = shallow(<App />);
+		expect(component).toMatchSnapshot();
+	});
+});
