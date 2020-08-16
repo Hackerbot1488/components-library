eb6947767297c2cbee1759f057e75ed91daa3bf9
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-16 22:06:58 +0300

    done button tests

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -23,6 +23,10 @@ describe("Button component", () => {
 			const component = shallow(<Button className="lol" />);
 			expect(component.find(".lol").length).toBe(1);
 		});
+		it('should render Button component as "a"', () => {
+			const component = shallow(<Button href="test" />);
+			expect(component.find("a").length).toBe(1);
+		});
 		it("should use custom handler", () => {
 			const mockCallback = jest.fn();
 			const component = shallow(<Button onCLick={mockCallback} />);
@@ -30,6 +34,17 @@ describe("Button component", () => {
 			expect(mockCallback.mock.calls.length).toBe(0);
 			btn.simulate("click");
 			expect(mockCallback.mock.calls.length).toBe(1);
+		});
+		it("should not to handle if disabled attribute is defined", () => {
+			const mockCallback = jest.fn();
+			const component = shallow(
+				<Button onCLick={mockCallback} disabled="true" />
+			);
+			const btn = component.find("button");
+			const event = new MouseEvent("click");
+			expect(mockCallback.mock.calls.length).toBe(0);
+			btn.simulate("click", event);
+			expect(mockCallback.mock.calls.length).toBe(0);
 		});
 	});
 });
