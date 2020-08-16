5239be59053191829715a8c10f7c2e2d2fc99e2e
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-16 19:27:16 +0300

    added button component

---
+++ src/components/Button/Button.spec.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button } from "./Button";
+import { shallow } from "enzyme";
+describe("Button component", () => {
+	it("should render Button component", () => {});
+});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import classNames from "class-names";
+import "./Button.css";
 
-export const Button: React.FC = () => {
+interface buttonProps {
+	children?: React.ReactNode | string;
+	onCLick?: () => void;
+	className?: string;
+	disabled?: boolean;
+	active?: boolean;
+}
+
+export const Button: React.FC<buttonProps> = ({
+	children,
+	onCLick,
+	className,
+	disabled,
+	active,
+}) => {
+	const classes: string = classNames("button", className, { active });
 	return (
 		<div>
-			<button>Button</button>
+			<button className={classes} disabled={disabled} onClick={onCLick}>
+				{children}
+			</button>
 		</div>
 	);
 };
-Button.defaultProps = {};
+Button.defaultProps = {
+	children: "Button",
+	className: "",
+	disabled: false,
+	onCLick: () => {},
+	active: false,
+};
