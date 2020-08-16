02f7471e72894d2ce08be2e3c899e22c279fdcc4
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-16 16:21:05 +0300

    deleted useless files and rename test files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import React from "react";
-import App from "./App";
-describe("App component", () => {
-	it("should render App component", () => {
-		const component = shallow(<App />);
-		expect(component).toMatchSnapshot();
-	});
-});
