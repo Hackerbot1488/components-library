b5019f917d80383e2a8c0e6baa7eb0a72576e0d8
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-16 16:22:01 +0300

    added footer component and tests for it

---
+++ src/layout/Footer/Footer.spec.tsx
@@ -0,0 +1,9 @@
+import { shallow } from "enzyme";
+import React from "react";
+import { Footer } from "./Footer";
+describe("Footer component", () => {
+	it("should render Footer component", () => {
+		const component = shallow(<Footer />);
+		expect(component).toMatchSnapshot();
+	});
+});
---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer: React.FC<{}> = () => (
+	<div className="footer">Created with love</div>
+);
