81b7f0b6a63c34bee5576133d8dfe9dac671b428
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-20 12:18:52 +0300

    added tababr nav

--- src/components/TabBar/TabBarNav/TabBarNav.tsx
+++ src/components/TabBar/TabBarNav/TabBarNav.tsx
@@ -3,12 +3,31 @@ import "./TabBarNav.css";
 import classNames from "class-names";
 interface TabBarNavProps {
 	className?: string;
+	navLabel?: string;
+	onChangeActiveTab?: (navLabel: string | undefined) => void;
 	[type: string]: any;
 }
 export const TabBarNav: React.FC<TabBarNavProps> = ({
 	className,
+	navLabel,
+	onChangeActiveTab,
 	...attrs
 }) => {
-	const classes = classNames("TabBarNav", classNames);
-	return <div className={classes} {...attrs}></div>;
+	const classes = classNames("nav-item", className);
+	return (
+		<button
+			className={classes}
+			onClick={() => {
+				onChangeActiveTab!(navLabel);
+			}}
+			{...attrs}
+		>
+			{navLabel}
+		</button>
+	);
+};
+TabBarNav.defaultProps = {
+	className: "",
+	navLabel: "Tab",
+	onChangeActiveTab: () => {},
 };
